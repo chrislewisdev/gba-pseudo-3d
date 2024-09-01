3f04e48fe8da26872223d7b0068de905e9963237
Chris Lewis-Hou <chris@lewis-hou.dev>
2024-09-01 15:19:36 +1000

    Test/fix camera movement

--- src/bg3d.cpp
+++ src/bg3d.cpp
@@ -12,7 +12,7 @@ namespace nos {
 
     void bg3d::update(nos::camera3d& camera) {
         vec3 bg_position = position - camera.get_position() * camera.get_world_transform() * camera.get_scale();
-        bg.set_position(bg_position.x, bg_position.y + bn::degrees_lut_sin(camera.get_pitch()) * camera.get_scale() * -height);
+        bg.set_position(bg_position.x, -bg_position.y + bn::degrees_lut_sin(camera.get_pitch()) * camera.get_scale() * -height);
         bg.set_mat_attributes(camera.get_affine_transform_xy());
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,12 @@ int main()
     nos::o3d npc(bn::sprite_items::fred_side_profile);
     nos::bg3d floor(bn::affine_bg_items::garden_floor);
     nos::bg3d ceiling(bn::affine_bg_items::garden_ceiling, 32);
-    int heading = 0, pitch = 30;
 
-    player.set_position(nos::vec3(0, 0, 48));
+    bool control_position = false;
+    int heading = 0, pitch = 30;
+    nos::vec3 camera_position;
+
+    player.set_position(nos::vec3(0, 0, 16));
     npc.set_position(nos::vec3(50, 50, 16));
 
     // bn::sprite_ptr sprite = bn::sprite_items::fred_side_profile.create_sprite(50, 50);
@@ -30,20 +33,39 @@ int main()
     {
         // if (++heading >= 360) heading = 0;
 
-        if (bn::keypad::left_held()) {
-            heading += 1;
-        } else if (bn::keypad::right_held()) {
-            heading -= 1;
-        }
-        if (heading < 0) heading += 360;
-        if (heading > 360) heading -= 360;
-        if (bn::keypad::up_held() && pitch > 0) {
-            pitch -= 1;
-        } else if (bn::keypad::down_held() && pitch < 85) {
-            pitch += 1;
+        if (bn::keypad::l_pressed()) {
+            control_position = !control_position;
         }
 
-        camera.update_camera(nos::vec3::zero, pitch, heading, 1);
+        if (control_position) {
+            if (bn::keypad::left_held()) {
+                camera_position.x -= 1;
+            } else if (bn::keypad::right_held()) {
+                camera_position.x += 1;
+            }
+            if (bn::keypad::up_held() && pitch > 0) {
+                camera_position.y -= 1;
+            } else if (bn::keypad::down_held() && pitch < 85) {
+                camera_position.y += 1;
+            }
+        } else {
+            if (bn::keypad::left_held()) {
+                heading += 1;
+            } else if (bn::keypad::right_held()) {
+                heading -= 1;
+            }
+            if (bn::keypad::up_held() && pitch > 0) {
+                pitch -= 1;
+            } else if (bn::keypad::down_held() && pitch < 85) {
+                pitch += 1;
+            }
+
+            // Clamp heading angle to [0..360] range
+            if (heading < 0) heading += 360;
+            if (heading > 360) heading -= 360;
+        }
+
+        camera.update_camera(camera_position, pitch, heading, 1);
         player.update(camera);
         npc.update(camera);
         floor.update(camera);
